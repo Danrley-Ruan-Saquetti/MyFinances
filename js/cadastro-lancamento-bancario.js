5741ae0a2346e3450af236847742cd85345380ac
Dan Ruan <danrsaquetti@gmail.com>
2025-06-30 21:46:09 -0300

    feat: Criando nova tela de Cadastro de Lançamentos Bancários

---
+++ js/cadastro-lancamento-bancario.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+	const lancamentoBancarioRepository = new LocalStorageRepository('lancamento-bancario')
+
+	const btnGravar = document.getElementById('btnGravar');
+	const btnCancelar = document.getElementById('btnCancelar');
+	const mensagem = document.getElementById('mensagem');
+
+	btnGravar.onclick = () => {
+
+	};
+}
