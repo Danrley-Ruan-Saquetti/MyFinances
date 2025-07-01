18981831fbb5379afe0b1342b958997cda130b56
Dan Ruan <danrsaquetti@gmail.com>
2025-06-30 22:08:59 -0300

    feat: Criando formulário de cadastro de Lançamento Bancário

--- js/cadastro-lancamento-bancario.js
+++ js/cadastro-lancamento-bancario.js
@@ -1,11 +1,37 @@
 window.onload = () => {
 	const lancamentoBancarioRepository = new LocalStorageRepository('lancamento-bancario')
+	const categoriaRepository = new LocalStorageRepository('categoria')
 
+	const listCategorias = document.getElementById('listCategorias');
 	const btnGravar = document.getElementById('btnGravar');
 	const btnCancelar = document.getElementById('btnCancelar');
 	const mensagem = document.getElementById('mensagem');
 
+	let tipoSelecionado = "Despesa";
+
 	btnGravar.onclick = () => {
 
 	};
+
+	document.querySelectorAll("[name='categoria']").forEach(categoriaOption => {
+		categoriaOption.addEventListener('click', () => {
+			tipoSelecionado = categoriaOption.value
+			renderCategorias()
+		})
+	})
+
+	function carregarCategorias() {
+		const categorias = categoriaRepository.getAll().filter(categoria => categoria.tipo == tipoSelecionado);
+
+		const listaCategorias = categorias.reduce((listaCategorias, categoria) => {
+			return listaCategorias + `<li class="list-group-item d-flex gap-1 bg-light-subtle text-white border border-secondary list-group-item-action" data-id="${categoria.id}">
+				<input class="form-check-input me-1" type="checkbox" value="${categoria.id}" id="categoria-${categoria.id}">
+    		<label class="form-check-label w-100" role="button" for="categoria-${categoria.id}">${categoria.nome}</label>
+			</li>`;
+		}, '');
+
+		listCategorias.innerHTML = `${listaCategorias}`;
+	}
+
+	carregarCategorias()
 }
